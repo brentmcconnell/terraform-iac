d2da5f3d02e8b9a6a55181051519524209c063ea
brentmcconnell <emconne@gmail.com>
2020-06-12 15:31:46 -0400

    refresh

--- data.tf
+++ data.tf
@@ -1,3 +1,3 @@
 data "azurerm_resource_group" "project-rg" {
-    name = "PROJECT-004-RG"
+    name = "PROJECT-947-RG"
 }
--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 locals {
   # All variables used in this file should be 
   # added as locals here 
-  prefix                = "${var.prefix}-004"
+  prefix                = "${var.prefix}-947"
   location              = var.location
   vault_name            = "${local.prefix}-vault"
   
