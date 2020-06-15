61f330b140470eb6173c68fd3151b1ee4a6f704d
brentmcconnell <emconne@gmail.com>
2020-06-14 21:45:48 -0400

    refresh

--- iac/backend.tf
+++ iac/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   required_version = ">=0.12.6"
   backend "azurerm" {
-    storage_account_name    = "tfstateacct2758"
+    storage_account_name    = "tfstateacct0551"
     container_name          = "proj-java"
     key                     = "terraform.tfstate"
-    access_key              = "w1IXhsbvh3tf+OKytbxhs76zqTR0vx6QdiFugRAB7m2TJwN9+/cHaNpLkt0pdV4lIwcMphYj4pFeZPFhCzHTfA=="
+    access_key              = "Zts4LNCq4VJOOTK3FAZOZwaZN3RzuwOozmbpOA9pSDGYNHLBtFXknU0ZtNuZ/eNzNxgAclNj4AH4CcOWGYEvCg=="
   }
 }
--- iac/data.tf
+++ iac/data.tf
@@ -1,3 +1,3 @@
 data "azurerm_resource_group" "project-rg" {
-    name = "PROJECT-2758-RG"
+    name = "PROJECT-0551-RG"
 }
