0809eacc40ccd09bcb030b81b9d510a38418e8ea
brentmcconnell <emconne@gmail.com>
2020-06-15 08:58:57 -0400

    refresh

--- iac/main.tf
+++ iac/main.tf
@@ -37,3 +37,50 @@ resource "azurerm_key_vault" "vault" {
   }
 
 }
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${local.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = "${azurerm_resource_group.main.location}"
+  resource_group_name   = data.azurerm_resource_group.project-rg.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name   = data.azurerm_resource_group.project-rg.name
+  virtual_network_name = "${azurerm_virtual_network.main.name}"
+  address_prefix       = "10.0.2.0/24"
+}
+
+resource "azurerm_linux_virtual_machine_scale_set" "main" {
+  name                            = "${local.prefix}-vmss"
+  resource_group_name             = data.azurerm_resource_group.project-rg.name
+  location                        = azurerm_resource_group.main.location
+  sku                             = "Standard_F2"
+  instances                       = 3
+  admin_username                  = "adminuser"
+  admin_password                  = "P@ssw0rd1234!"
+  disable_password_authentication = false
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  network_interface {
+    name    = "example"
+    primary = true
+
+    ip_configuration {
+      name      = "internal"
+      primary   = true
+      subnet_id = azurerm_subnet.internal.id
+    }
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
