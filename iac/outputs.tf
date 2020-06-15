37625b8bb8a361ba108800fa4d240afebec8a539
brentmcconnell <emconne@gmail.com>
2020-06-15 10:56:33 -0400

    refresh

--- iac/outputs.tf
+++ iac/outputs.tf
@@ -9,3 +9,27 @@ output "prefix" {
 output "proj-rg-id" {
   value = data.azurerm_resource_group.project-rg.id
 }
+
+output "id" {
+  value = azurerm_kubernetes_cluster.kubernetes.id
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.kubernetes.kube_config_raw
+}
+
+output "client_key" {
+  value = azurerm_kubernetes_cluster.kubernetes.kube_config.0.client_key
+}
+
+output "client_certificate" {
+  value = azurerm_kubernetes_cluster.kubernetes.kube_config.0.client_certificate
+}
+
+output "cluster_ca_certificate" {
+  value = azurerm_kubernetes_cluster.kubernetes.kube_config.0.cluster_ca_certificate
+}
+
+output "host" {
+  value = azurerm_kubernetes_cluster.kubernetes.kube_config.0.host
+}
