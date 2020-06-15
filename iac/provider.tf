bf8ae8a95435d86528d95cd0c74b31594bf5ee6a
brentmcconnell <emconne@gmail.com>
2020-06-14 20:54:02 -0400

    refresh

--- iac/provider.tf
+++ iac/provider.tf
@@ -1,7 +1,7 @@
 provider "azurerm" {
   version                   = ">=2.0.0"
   client_id                 = "${var.clientId}"
-  client_certificate_path   = "service-principal.pfx"
+  client_certificate_path   = "../cert/service-principal.pfx"
   subscription_id           = "${var.subscriptionId}"
   tenant_id                 = "${var.tenantId}"
   features {} 
