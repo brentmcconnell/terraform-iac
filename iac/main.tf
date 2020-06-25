d959818840379629dd11e71b57ba28717a23233d
brentmcconnell <emconne@gmail.com>
2020-06-25 16:45:38 -0400

    changed to appservice

--- iac/main.tf
+++ iac/main.tf
@@ -12,64 +12,19 @@ locals {
 
 data "azurerm_client_config" "current" {}
 
-resource "azurerm_virtual_network" "vnet" {
-  name                = "${local.prefix}-network"
-  location            = data.azurerm_resource_group.project-rg.location 
+resource "azurerm_app_service_plan" "slotDemoPlan" {
+  name                = "AppServicePlan-${local.prefix}"
+  location            = local.location
   resource_group_name = data.azurerm_resource_group.project-rg.name
-  address_space       = ["10.1.0.0/16"]
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
 }
 
-resource "azurerm_subnet" "internal" {
-  name                 = "internal"
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  resource_group_name  = data.azurerm_resource_group.project-rg.name
-  address_prefixes     = ["10.1.0.0/22"]
-}
-
-resource "azurerm_kubernetes_cluster" "kubernetes" {
-  name                = "${local.prefix}-k8s"
-  dns_prefix          = "${local.prefix}-k8s"
-  location            = data.azurerm_resource_group.project-rg.location 
+resource "azurerm_app_service" "slotDemoService" {
+  name                = "AppService-${local.prefix}"
+  location            = local.location
   resource_group_name = data.azurerm_resource_group.project-rg.name
-
-  default_node_pool {
-    name           = "system"
-    node_count     = 1
-    vm_size        = "Standard_DS2_v2"
-    vnet_subnet_id = azurerm_subnet.internal.id
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  addon_profile {
-    aci_connector_linux {
-      enabled = false
-    }
-
-    azure_policy {
-      enabled = false
-    }
-
-    http_application_routing {
-      enabled = false
-    }
-
-    kube_dashboard {
-      enabled = true
-    }
-
-    oms_agent {
-      enabled = false
-    }
-  }
-}
-
-resource "azurerm_kubernetes_cluster_node_pool" "user" {
-  name                  = "user"
-  kubernetes_cluster_id = azurerm_kubernetes_cluster.kubernetes.id
-  vm_size               = "Standard_DS2_v2"
-  node_count            = 1
-  vnet_subnet_id        = azurerm_subnet.internal.id
+  app_service_plan_id = "${azurerm_app_service_plan.slotDemoPlan.id}"
 }
