34a6ba10c5abc48ff147cffad8a55f860bb49bd8
brentmcconnell <emconne@gmail.com>
2020-06-25 17:04:30 -0400

    pipeline chagnes

--- iac/outputs.tf
+++ iac/outputs.tf
@@ -10,26 +10,3 @@ output "proj-rg-id" {
   value = data.azurerm_resource_group.project-rg.id
 }
 
-output "id" {
-  value = azurerm_kubernetes_cluster.kubernetes.id
-}
-
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.kubernetes.kube_config_raw
-}
-
-output "client_key" {
-  value = azurerm_kubernetes_cluster.kubernetes.kube_config.0.client_key
-}
-
-output "client_certificate" {
-  value = azurerm_kubernetes_cluster.kubernetes.kube_config.0.client_certificate
-}
-
-output "cluster_ca_certificate" {
-  value = azurerm_kubernetes_cluster.kubernetes.kube_config.0.cluster_ca_certificate
-}
-
-output "host" {
-  value = azurerm_kubernetes_cluster.kubernetes.kube_config.0.host
-}
