bbeb0afdf95f3d4c521e6bc88cec496f264ea4fc
Brent McConnell <brmcconn@microsoft.com>
2020-06-26 13:01:32 -0400

    Update backend.tf

--- iac/backend.tf
+++ iac/backend.tf
@@ -4,6 +4,6 @@ terraform {
     storage_account_name    = "tfstateacct2141"
     container_name          = "tfcont-2141"
     key                     = "terraform.tfstate"
-    access_key              = "o7BgPP7DOJ3WQPKrSWQAc8z6iWx9E9UaviyWiCB85+V8AdKnYT7P0bt2wPsZ+vWFnpRskQ0B4ycL6M/HVgl/qQ=="
+    access_key              = "xxxxxxxxxxxxxxxxxxxx+V8AdKnYT7P0bt2wPsZ+vWFnpRskQ0B4ycL6M/HVgl/qQ=="
   }
 }
